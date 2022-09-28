73d8fdd8bfe3250e0d6653866a498ce68c71b2d6
Akanksha Kushwaha <cmakanksha@gmail.com>
2022-09-28 22:27:20 +0530

    Feat: updated deployment

--- src/components/project.js
+++ src/components/project.js
@@ -102,7 +102,7 @@ class Project extends Component {
                                         this.setState({ modalDisplay: "none" });
                                         document.body.style.overflow = "initial";
                                     }}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 172 172" style={{ "fill": "rgba(0, 0, 0, 0.87)" }}>
+                                        <svg xmlns="https://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 172 172" style={{ "fill": "rgba(0, 0, 0, 0.87)" }}>
                                             <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style={{ "mix-blend-mode": "normal" }}>
                                                 <path d="M0,172v-172h172v172z" fill="none"></path>
                                                 <g fill="rgba(0, 0, 0, 0.87)">
@@ -131,7 +131,7 @@ class Project extends Component {
                     <div className={this.state.scrollStamp > 690 ? "p-search-section search-scrolled" : "p-search-section" }>
                         <div className="p-search">
                             <div className="p-search-input">
-                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 172 172" style={{ "fill":"rgba(0, 0, 0, 0.87)"}}>
+                                <svg xmlns="https://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 172 172" style={{ "fill":"rgba(0, 0, 0, 0.87)"}}>
                                     <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style={{ "mix-blend-mode": "normal"}}>
                                     <path d="M0,172v-172h172v172z" fill="none"></path>
                                         <g style={{ "fill": "#fff"}}>
